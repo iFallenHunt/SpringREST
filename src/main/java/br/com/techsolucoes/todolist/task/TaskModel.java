b9c527ae14ec927a5e6c4c0fdfc1fe61eb383a92
Lucas Santos <43353219+iFallenHunt@users.noreply.github.com>
2023-10-15 19:12:07 -0300

    Dockerfile deploy

---
+++ src/main/java/br/com/techsolucoes/todolist/task/TaskModel.java
@@ -0,0 +1,45 @@
+package br.com.techsolucoes.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+//import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_task")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    // 2 - aqui eu defini que quem é responsavel pela excessao é quem esta chamando
+    // o metodo title
+    public void setTitle(String title) throws Exception {
+        if (title.length() > 50) {
+            // 1 - aqui eu criei a excessao
+            throw new Exception("O Campo title deve conter no máximo 50 caracteres");
+        }
+        this.title = title;
+    }
+
+}
---
+++ src/main/java/br/com/techsolucoes/todolist/user/UserContoller.java
@@ -0,0 +1,38 @@
+package br.com.techsolucoes.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+@RestController
+@RequestMapping("/users")
+public class UserContoller {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario ja existe");
+        }
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        // $2a$12$US00g/uMhoSBm.HiuieBjeMtoN69SN.GE25fCpldebzkryUyopws6
+
+        userModel.setPassword(passwordHashred);
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.OK).body(userCreated);
+
+    }
+
+}
